ca45c43bc7dc6807a5c4c5dac5365db0c61fc795
Helon21 <helonbentes51@hotmail.com>
2024-03-27 16:28:24 -0300

    create class chessMatch, UI, Color, ChessPiece

--- src/Application/Program.java
+++ src/Application/Program.java
@@ -1,9 +1,12 @@
-package Application;
+package application;
+
+import chess.ChessMatch;
 
 public class Program {
 
     public static void main(String[] args) {
-
+        ChessMatch chessMatch = new ChessMatch();
+        UI.printBoard(chessMatch.getPieces());
     }
 
 }
